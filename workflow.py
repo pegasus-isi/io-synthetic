8d1343dc3497844f204963db6670a27da02b5b11
Loïc Pottier <48072795+lpottier@users.noreply.github.com>
2021-06-23 16:17:55 -0700

    new version with chain, fork and custom
    Signed-off-by: Loïc Pottier <48072795+lpottier@users.noreply.github.com>

--- workflow.py
+++ workflow.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
+from Pegasus.api import *
+
 import os
+import sys
 import logging
 import subprocess
+import yaml
 from pathlib import Path
 from datetime import datetime
 from argparse import ArgumentParser
+from typing import Optional, Tuple, Union
 
 logging.basicConfig(level=logging.DEBUG)
 
 # --- Import Pegasus API ------------------------------------------------------
-from Pegasus.api import *
+
 
 class IOSyntheticWorkflow(object):
     wf = None
@@ -22,11 +27,23 @@ class IOSyntheticWorkflow(object):
     wf_dir = None
 
     # --- Init ----------------------------------------------------------------
-    def __init__(self, dagfile="workflow.yml"):
-        self.dagfile = dagfile
+    def __init__(self,
+                 shape: Optional[Tuple[str, Union[int, str]]] = ("chain", 1),
+                 exec_site_name: Optional[str] = "condorpool"
+                 ) -> None:
         self.wf_name = "io-synthetic"
-        self.wid = "io-synth-" + datetime.now().strftime("%s")
+        self.wid = self.wf_name + "-" + datetime.now().strftime("%s")
+        self.dagfile = self.wid+".yml"
+
         self.wf_dir = str(Path(__file__).parent.resolve())
+        self.exec_site_name = exec_site_name
+        self.shape = shape            
+
+        self.shared_scratch_dir = os.path.join(
+            self.wf_dir, "{}/scratch".format(self.wid))
+        self.local_storage_dir = os.path.join(
+            self.wf_dir, "{}/output".format(self.wid))
+
 
     # --- Write files in directory --------------------------------------------
     def write(self):
@@ -45,11 +62,8 @@ class IOSyntheticWorkflow(object):
         return
 
     # --- Site Catalog --------------------------------------------------------
-    def create_sites_catalog(self, exec_site_name="condorpool"):
+    def create_sites_catalog(self):
         self.sc = SiteCatalog()
-
-        self.shared_scratch_dir = os.path.join(self.wf_dir, "{}/scratch".format(self.wid))
-        self.local_storage_dir = os.path.join(self.wf_dir, "{}/output".format(self.wid))
 
         local = Site("local").add_directories(
             Directory(Directory.SHARED_SCRATCH, self.shared_scratch_dir).add_file_servers(
@@ -61,7 +75,7 @@ class IOSyntheticWorkflow(object):
         )
 
         exec_site = (
-            Site(exec_site_name)
+            Site(self.exec_site_name)
             .add_pegasus_profile(style="condor")
             .add_condor_profile(universe="vanilla")
             .add_profiles(Namespace.PEGASUS, key="data.configuration", value="condorio")
@@ -70,7 +84,7 @@ class IOSyntheticWorkflow(object):
         self.sc.add_sites(local, exec_site)
 
     # --- Transformation Catalog (Executables and Containers) -----------------
-    def create_transformation_catalog(self, exec_site_name="condorpool"):
+    def create_transformation_catalog(self):
         self.tc = TransformationCatalog()
 
         # --- Transformations ---------------------------------------------------------------
@@ -94,44 +108,138 @@ class IOSyntheticWorkflow(object):
 
         self.tc.add_transformations(keg)
 
+
     # --- Create Workflow -----------------------------------------------------
+
     def create_workflow(self):
+        if self.shape[0] == "chain":
+            self.create_workflow_chain()
+        elif self.shape[0] == "fork":
+            self.create_workflow_fork()
+        else:
+            self.create_workflow_custom()
+
+    def create_workflow_chain(self):
         self.wf = Workflow(self.wf_name, infer_dependencies=True)
 
-        # -o namefile1 namefile2 -G sizefile1 sizefile2 -u M
-        k1_out = File("k1.txt")
-
-        keg_job1 = (
+        ## First job
+        f1 = File("j1.txt")
+        job1 = (
             Job("keg")
-            .add_args("-o", k1_out, "-T", 5, "-G", 100, "-u", "M")
-            .add_outputs(k1_out, stage_out=True, register_replica=False)
+            .add_args("-o", f1, "-T", 5, "-G", 100, "-u", "M")
+            .add_outputs(f1, stage_out=False, register_replica=False)
+        )
+        self.wf.add_jobs(job1)
+
+        # Security check to ensure nb of jobs is positive >= 1
+        nb_jobs = max(self.shape[1], 1)
+
+        for i in range(1, nb_jobs):
+            fi = File("f{}.txt".format(i))
+            keg = (
+                Job("keg")
+                .add_args("-i", f1, "-o", fi, "-T", 5, "-G", 100, "-u", "M")
+                .add_inputs(f1)
+                .add_outputs(fi, stage_out=False, register_replica=False)
+            )
+            f1 = fi
+            self.wf.add_jobs(keg)
+
+    def create_workflow_fork(self):
+        self.wf = Workflow(self.wf_name, infer_dependencies=True)
+
+        ## First job
+        f1 = File("j1.txt")
+        job1 = (
+            Job("keg")
+            .add_args("-o", f1, "-T", 5, "-G", 100, "-u", "M")
+            .add_outputs(f1, stage_out=False, register_replica=False)
+        )
+        self.wf.add_jobs(job1)
+
+        # Security check to ensure nb of jobs is positive >= 1
+        nb_jobs = max(self.shape[1], 1)
+
+        flast = File("f{}.txt".format(nb_jobs+1))
+        joblast = (
+            Job("keg")
+            .add_args("-o", flast, "-T", 5, "-G", 100, "-u", "M")
+            .add_outputs(flast, stage_out=False, register_replica=False)
         )
 
-        k2_out = File("k2.txt")
+        for i in range(1, nb_jobs+1):
+            fi = File("f{}.txt".format(i))
+            keg = (
+                Job("keg")
+                .add_args("-i", f1, "-o", fi, "-T", 5, "-G", 100, "-u", "M")
+                .add_inputs(f1)
+                .add_outputs(fi, stage_out=False, register_replica=False)
+            )
 
-        keg_job2 = (
-            Job("keg")
-            .add_args("-i", k1_out, "-o", k2_out, "-T", 5, "-G", 100, "-u", "M")
-            .add_inputs(k1_out)
-            .add_outputs(k2_out, stage_out=True, register_replica=False)
-        )
+            joblast.add_args("-i", fi)
+            joblast.add_inputs(fi)
 
+            self.wf.add_jobs(keg)
 
-        self.wf.add_jobs(keg_job1, keg_job2)
+        ## We add the last job
+        self.wf.add_jobs(joblast)
+
+    # --- Create Workflow -----------------------------------------------------
+    def create_workflow_custom(self):
+        self.wf = Workflow(self.wf_name, infer_dependencies=True)
+
+        file = self.shape[1]
+
+        with open(file, 'r') as f:
+            try:
+                data = yaml.safe_load(f)
+
+                for job in data['jobs']:
+                    keg = Job("keg")
+                    keg.add_args("-G", 100, "-u", "M")
+
+                    for lfn in job['uses']:
+                        fi = File(lfn['lfn'])
+
+                        if lfn['type'] == "input":
+                            keg.add_inputs(fi)
+                            keg.add_args("-i", fi)
+                        else:
+                            keg.add_outputs(fi, stage_out=lfn['stageOut'],
+                                            register_replica=lfn['registerReplica'])
+                            keg.add_args("-o", fi)
+                    
+                    self.wf.add_jobs(keg)
+
+            except yaml.YAMLError as e:
+                print(e)
+                sys.exit(-1)
+
 
     # --- Run Workflow -----------------------------------------------------
-    def run(self, submit=False):
+    def run(self, submit=False, wait=False):
         try:
-            self.wf.plan(
-                dir=self.wf_dir,
-                relative_dir=self.wid,
-                output_sites=["local"],
-                output_dir=self.local_storage_dir,
-                cleanup="leaf",
-                submit=submit
-            )  # .wait()
+            if wait:
+                self.wf.plan(
+                    dir=self.wf_dir,
+                    relative_dir=self.wid,
+                    output_sites=["local"],
+                    output_dir=self.local_storage_dir,
+                    cleanup="leaf",
+                    submit=True
+                ).wait()
+            else:
+                self.wf.plan(
+                    dir=self.wf_dir,
+                    relative_dir=self.wid,
+                    output_sites=["local"],
+                    output_dir=self.local_storage_dir,
+                    cleanup="leaf",
+                    submit=submit
+                )
         except Exception as e:
             print(e)
+            sys.exit(-1)
 
 
 if __name__ == "__main__":
@@ -139,43 +247,88 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "-s",
-        "--skip_sites_catalog",
+        "--skip-sites-catalog",
         action="store_true",
         help="Skip site catalog creation",
     )
     parser.add_argument(
         "-e",
-        "--execution_site_name",
+        "--execution-site",
         metavar="STR",
         type=str,
         default="condorpool",
         help="Execution site name (default: condorpool)",
     )
     parser.add_argument(
-        "-o",
-        "--output",
+        "-c",
+        "--workflow-class",
         metavar="STR",
         type=str,
-        default="workflow.yml",
-        help="Output file (default: workflow.yml)",
+        default="chain",
+        required=True,
+        help="Workflow structure, chain, fork or custom (default: chain)",
     )
+    parser.add_argument(
+        "-w",
+        "--workflow-yml",
+        metavar="STR",
+        type=str,
+        default=None,
+        help="YAML file of an existing Pegasus workflow",
+    )
+    parser.add_argument(
+        "-n",
+        "--number-jobs",
+        metavar="INT",
+        type=int,
+        default=1,
+        help="Number of jobs generated, only valid if chain or fork have been chosen (default: 1)",
+    )
+
+    # parser.add_argument(
+    #     "-o",
+    #     "--output",
+    #     metavar="STR",
+    #     type=str,
+    #     default="workflow.yml",
+    #     help="Output file (default: workflow.yml)",
+    # )
 
     args = parser.parse_args()
 
-    workflow = IOSyntheticWorkflow(args.output)
+    if not args.workflow_class in ["chain", "fork", "custom"]:
+        parser.error('-c/--workflow-class can only be set to "chain", "fork" or "custom".')
+    
+    if args.workflow_class == "custom" and args.workflow_yml is None:
+        parser.error(
+            '-c/--workflow-class == "custom" requires -w/--workflow-yml to be set.')
+
+    if args.workflow_class in ["chain", "fork"] and (args.number_jobs is None or args.number_jobs < 1):
+        parser.error(
+            '-c/--workflow-class == "chain" or "fork" requires -n/--number-jobs to be set (and it must be >=1).')
+
+    if args.workflow_class == "custom":
+        workflow_class = (args.workflow_class, args.workflow_yml)
+    elif args.workflow_class in ["chain", "fork"]:
+        workflow_class = (args.workflow_class, args.number_jobs)
+    else:
+        parser.error('Unknown parsing argument error')
+
+    workflow = IOSyntheticWorkflow(
+        exec_site_name=args.execution_site, shape=workflow_class)
 
     if not args.skip_sites_catalog:
         print("Creating execution sites...")
-        workflow.create_sites_catalog(args.execution_site_name)
+        workflow.create_sites_catalog()
 
     print("Creating workflow properties...")
     workflow.create_pegasus_properties()
-    
+
     print("Creating transformation catalog...")
-    workflow.create_transformation_catalog(args.execution_site_name)
+    workflow.create_transformation_catalog()
 
     print("Creating pipeline workflow dag...")
     workflow.create_workflow()
 
     workflow.write()
-    workflow.run(submit=False)
+    workflow.run(submit=False, wait=False)

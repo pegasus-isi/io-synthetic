463bd850c22d4ec571af46302b5b70dad4708870
Tu Mai Anh Do <tudo@usc.edu>
2021-09-07 15:55:41 -0700

    generate_decaf_cumulative_runtime function

--- analysis/process.sh
+++ analysis/process.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
+LOG_DIR=./log
 function generate_statistics {
-    for d in */ ; do
+    for d in ${LOG_DIR}/decaf-*/ ; do
         echo "$d"
         pegasus-statistics -s all ${d}
     done
 }
 
 function generate_cumulative_runtime {
-    for d in */ ; do
+    for d in ${LOG_DIR}/*/ ; do
         ctime=$(cat ${d}/statistics/breakdown.txt | grep -m1 keg | awk -F' ' '{print $7}')
+        echo "$d ${ctime}"
+    done
+}
+
+function generate_decaf_cumulative_runtime {
+    for d in ${LOG_DIR}/decaf-*/ ; do
+        ctime=$(cat ${d}/00/00/merge_cluster1.out | sed -n '3 p' | sed 's/.*is \([^ ]*\)[ ]*.*/\1/')
         echo "$d ${ctime}"
     done
 }
 
 function get_stats {
     # cat stat | grep $1 | awk -F' ' '{print $2}'
-    cat stat | grep $1 | awk '{if($2!=""){count++;sum+=$2};y+=$2^2} END{sq=sqrt(y/NR-(sum/NR)^2);sq=sq?sq:0;print "mean = "sum/count ORS "std = ",sq}'
+    cat decaf_stat | grep $1 | awk '{if($2!=""){count++;sum+=$2};y+=$2^2} END{sq=sqrt(y/NR-(sum/NR)^2);sq=sq?sq:0;print "mean = "sum/count ORS "std = ",sq}'
 }
 
-opts=( "nosleep" "sleep" )
-sizes=( 1 2 4 8 )
+# generate_statistics
+# generate_cumulative_runtime
+opts=( "sleep" "sleep" )
+sizes=( 1 2 4 8 16 )
 for opt in "${opts[@]}"; do
     for size in "${sizes[@]}"; do
         echo ${size}g-${opt}
         get_stats ${size}g-${opt}
     done
 done
-
+# generate_decaf_cumulative_runtime
